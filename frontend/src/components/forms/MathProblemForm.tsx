a897a65a70f5b4293aa4e0555e310660f0fd7b3f
7i6ht <ruhstals@student.ethz.ch>
2025-09-28 14:05:07 +0200

    Hint input: Align font size to MWP input

--- frontend/src/components/forms/MathProblemForm.tsx
+++ frontend/src/components/forms/MathProblemForm.tsx
@@ -114,7 +114,7 @@ export const MathProblemForm = ({
               <FormItem>
                 <FormControl>
                   <textarea
-                    className={`w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${largeFont ? 'md:text-md md:leading-relaxed lg:text-l' : ''}`}
+                    className={`w-full rounded-md border border-input bg-background px-3 py-2 text-base md:text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${largeFont ? 'md:text-md md:leading-relaxed lg:text-l' : ''}`}
                     placeholder="Add hints about the relationships between the visual elements ..."
                     rows={rows}
                     spellCheck={false}
